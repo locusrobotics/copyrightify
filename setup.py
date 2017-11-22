d1806b0d945154dacdc1b54ed55c984dbddc5394
Paul Bovbel <pbovbel@locusrobotics.com>
2017-11-22 14:07:44 -0500

    0.0.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup
 
 setup(name='copyrightify',
       packages=['copyrightify'],
-      version='0.0.1',
+      version='0.0.2',
       description='Apply copyright/license headers to source files',
       author='Paul Bovbel',
       author_email='pbovbel@locusrobotics.com',
